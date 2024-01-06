efd47d3fda13720948c8f45d5e6e5b9961ae87f6
Nireep V <nireepv@Nireeps-MacBook-Pro.local>
2024-01-06 10:40:14 +1030

    modifying homepage design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
-import './Style/Home.css'
+import React, { useState, useEffect } from 'react';
+import './style/Home.css'
 import myPicture from '../assets/myHeadshot.png';
 
+function formatTime(date) {
+  return date.toLocaleTimeString('en-AU', {
+    timeZone: 'Australia/Adelaide',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  });
+}
+
 const Home = () => {
+  const [time, setTime] = useState(formatTime(new Date()));
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(formatTime(new Date()));
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className='mainDiv'>
-      <img className='myHeadshot' src={myPicture} alt="Me" />
-      <h2 className="centeredText">Nireep Vishnubhatla</h2>
-      <p>This is my Portfolio website.</p>
+      <div id='timeDiv'>
+        <h4 id='time'>{time}</h4>
+      </div>
+      <div id='photoDiv'>
+        <img className='myHeadshot' src={myPicture} alt="Me" />
+      </div>
+      <div className='body'>
+        <div><h2 className="centeredText">Nireep Vishnubhatla</h2></div>
+        <div id='socialDiv'>
+          <li className='socials'>github</li>
+          <li className='socials'>linkdin</li>
+          <li className='socials'>instagram</li>  
+        </div>
+        <div>
+        </div>
+        <div><h4 className="centeredText">Coordinates</h4></div>
+      </div>
     </div>
   );
 }
