9b3390a825134a91957b87150d8506fd8fe30dc4
NireepV <nireep.vishnubhatla@gmail.com>
2024-01-10 04:22:27 +1030

    added icon highlighting functionality to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,55 @@
 import './Navbar.css';
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useLocation } from "react-router-dom";
 
+const Navbar= () => {
+  const [highlightedIcon, setHighlightedIcon] = useState(null);
+  const location = useLocation();
 
-const Navbar= () =>{
   return (
     <div className='NavBarItems'>
       <li>
-        <Link to="/"><img src="src/assets/HomeUnselected.png" className='NavIcon'/></Link>
+        <Link to="/">
+          <img 
+            src={location.pathname === '/' || highlightedIcon === 'home' ? "src/assets/Home.png" : "src/assets/HomeUnselected.png"} 
+            className={`NavIcon ${highlightedIcon === 'home' ? 'highlighted' : ''}`} 
+            onMouseEnter={() => setHighlightedIcon('home')}
+            onMouseLeave={() => setHighlightedIcon(null)}
+          />
+        </Link>
       </li>
       <li>
-        <Link to="/AboutMe"><img src="src/assets/AboutMeUnselected.png" className='NavIcon'/></Link>
+        <Link to="/AboutMe">
+          <img 
+            src={location.pathname === '/AboutMe' || highlightedIcon === 'aboutMe' ? "src/assets/AboutMe.png" : "src/assets/AboutMeUnselected.png"} 
+            className={`NavIcon ${highlightedIcon === 'aboutMe' ? 'highlighted' : ''}`} 
+            onMouseEnter={() => setHighlightedIcon('aboutMe')}
+            onMouseLeave={() => setHighlightedIcon(null)}
+          />
+        </Link>
       </li>
       <li>
-        <Link to="/Projects"><img src="src/assets/ProjectsUnselected.png" className='NavIcon'/></Link>
+        <Link to="/Projects">
+          <img 
+            src={location.pathname === '/Projects' || highlightedIcon === 'projects' ? "src/assets/Projects.png" : "src/assets/ProjectsUnselected.png"} 
+            className={`NavIcon ${highlightedIcon === 'projects' ? 'highlighted' : ''}`} 
+            onMouseEnter={() => setHighlightedIcon('projects')}
+            onMouseLeave={() => setHighlightedIcon(null)}
+          />
+        </Link>
       </li>
       <li>
-        <Link to="/ContactMe"><img src="src/assets/ContactMeUnselected.png" className='NavIcon'/></Link>
+        <Link to="/ContactMe">
+          <img 
+            src={location.pathname === '/ContactMe' || highlightedIcon === 'contactMe' ? "src/assets/ContactMe.png" : "src/assets/ContactMeUnselected.png"} 
+            className={`NavIcon ${highlightedIcon === 'contactMe' ? 'highlighted' : ''}`} 
+            onMouseEnter={() => setHighlightedIcon('contactMe')}
+            onMouseLeave={() => setHighlightedIcon(null)}
+          />
+        </Link>
       </li>
     </div>
   );
 }
+
 export default Navbar;
