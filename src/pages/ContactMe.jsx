9bfd6cee9bf6285981de2aec552d07163a00e43b
NireepV <nireep.vishnubhatla@gmail.com>
2024-01-14 14:57:24 +1030

    getting contact me page started

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './style/ContactMe.css';
+
 const ContactMe = () =>{
   return (
     <div>
-      <h1>Goat</h1>
+      <h1>Contact Me</h1>
       <p>This is a paragraph</p>
     </div>
   );
