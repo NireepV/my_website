8adf25f2606be7bc2e34c85bf02962d557467640
NireepV <nireep.vishnubhatla@gmail.com>
2024-01-16 18:21:09 +1030

    Finally got contact page working the way I want

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -5,6 +5,15 @@ const ContactMe = () =>{
   const [isIntersecting, setIsIntersecting] = useState(false);
   const ref = useRef();
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [formValues, setFormValues] = useState({ email: '', subject: '', content: '' });
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleInputChange = (event) => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -36,15 +45,25 @@ const ContactMe = () =>{
         <h3 className={`centeredText ${scrollPosition > 50 ? 'small2' : 'large2'}`}>Let's Get To Know Each Other</h3>
       </div>
 
-      <div className={`contact ${scrollPosition > 50 ? 'visible' : 'hidden'}`}>
-        <div className='details'>
-          <input type="text" name="email" id="email" placeholder="Email@Example.com"></input>
-          <input type="text" name="subject" id="subject" placeholder="A Very Important Subject"></input>
+      <div className={`bodyC ${scrollPosition > 50 ? 'visible' : 'hidden'}`}>
+        <div className='contact'>
+          <div className='details'>
+            <input type="text" name="email" id="email" placeholder="Email@Example.com" value={formValues.email} onChange={handleInputChange} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} />
+            <input type="text" name="subject" id="subject" placeholder="A Very Important Subject" value={formValues.subject} onChange={handleInputChange} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} />
+          </div>
+          <div className='textareaWrapper'>
+            <textarea name="content" id="content" cols="30" rows="10" placeholder="Type Away My Fellow Email Enthusist ....." value={formValues.content} onChange={handleInputChange} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} />
+          </div>
+          <button className='sendEmail' type='submit'>Send &#128228;</button>
         </div>
-        <div className='textareaWrapper'>
-          <textarea name="content" id="content" cols="30" rows="10" placeholder="Type Away My Fellow Email Enthusist ....."></textarea>
+        <div className='liveTracking'>
+          <h4 className='LTelement'>Watched Values Updated Live:</h4>
+          <p className='LTelement'>&#123;</p>
+          <p className='LTelement'> &emsp; <q>Email</q> : <q>{formValues.email}</q></p>
+          <p className='LTelement'> &emsp; <q>Subject</q> : <q>{formValues.subject}</q></p>
+          <p className='LTelement bodyContent'> &emsp; <q>Body</q> : <q>{formValues.content}</q></p>
+          <p className='LTelement'>&#125;</p>
         </div>
-        <button type='submit'>Send</button>
       </div>
     </div>
   );
